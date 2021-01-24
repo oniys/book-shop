a7cde953f2c4a764508724dc3eaf16629e07358e
mac <oniys@mail.com>
2021-01-24 20:22:11 +0200

    state new code #1 REDUX&ReactJS Books

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import {connect} from "react-redux";
+import { setBooks } from "./actions/books";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component{
+
+  render() {
+
+const {books} = this.props.books;
+const {setBooks} = this.props;
+const newBooks = [
+    {
+        id: 0,
+        title: 'New DATA Books'
+    }
+];
+    return <div>
+
+      <h1>{books[0].title }</h1>
+        <button onClick={setBooks.bind(this,newBooks)}>SET NEW BOOKS</button>
+    </div>;
+
+  }
 }
+const  mapStareToProps = state => ({
+  ...state
+});
 
-export default App;
+
+const mapDispatchToProps = dispatch => ({
+setBooks: books => dispatch(setBooks(books))
+});
+
+export default connect(mapStareToProps, mapDispatchToProps)(App);
